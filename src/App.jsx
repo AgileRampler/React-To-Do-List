67e1c6f465b386c247ac47fc0a2b4b03e956a360
AgileRampler <43984000+AgileRampler@users.noreply.github.com>
2026-01-09 03:27:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ToDo from './components/Todo.jsx'
 const App = () => {
   return (
-    <div  className='bg-stone-900 grid py-4 min-h-screen '>
+    <div  className='bg-stone-900 grid py-4 min-h-screen  max-sm:min-w-10  overflow-x-hidden'>
     <ToDo/>
     </div>
   )
