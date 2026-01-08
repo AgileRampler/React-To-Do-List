4c6766e3a6dd5e07728b14560d8e1b56d12f9362
AgileRampler <43984000+AgileRampler@users.noreply.github.com>
2026-01-08 20:35:19 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,93 +1,132 @@
-import React, { useEffect, useRef, useState } from 'react'
-import to_do from '../assets/checklist.gif'
-import Todoitems from "./Todoitems.jsx"
+import React, { useEffect, useRef, useState } from "react";
+import to_do from "../assets/checklist.gif";
+import Todoitems from "./Todoitems.jsx";
 
 function Todo() {
-  const inputRef = useRef();
-  const [todoList, setTodoList] = useState(localStorage.getItem("todos")?
-  JSON.parse(localStorage.getItem("todos")) : [])
+  const inputRef = useRef(null);
 
-    const add = () => {
-        const inputText = inputRef.current.value.trim();
-        console.log(inputText);
-        
-        if (inputText === "") {
-            return null;
-        }
+  // Load todos from localStorage
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
 
-       const newTodo = {
-        id: Date.now(),
-        text: inputText,
-        isCompelte:false,
+  // Add Todo
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
 
-       }
-       setTodoList ((prev)=>[...prev, newTodo]);
-       inputRef.current.value = "";
+    if (inputText === "") return;
 
-       
-      
-    
-    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isCompelte: false,
+    };
 
-   useEffect(()=>{
-    console.log(todoList);
-    
-   },[todoList])
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
 
-    const deleteTodo= (id)=>{
-        setTodoList((prvTodos)=>{
-             return prvTodos.filter((todo) => todo.id !== id)
-        })
+  // Delete Todo
+  const deleteTodo = (id) => {
+    setTodoList((prevTodos) =>
+      prevTodos.filter((todo) => todo.id !== id)
+    );
+  };
 
-    }
-     const toggle = (id)=>{
-        setTodoList((prevTodos)=>{
-            return prevTodos.map((todo)=>{
-                if(todo.id === id){
-                    return {...todo, isCompelte: !todo.isCompelte}
-                }
-                return todo;
-            })
-        })
+  // Toggle Complete
+  const toggle = (id) => {
+    setTodoList((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id
+          ? { ...todo, isCompelte: !todo.isCompelte }
+          : todo
+      )
+    );
+  };
 
-
-        useEffect(()=>{
-            localStorage.setItem("todos",JSON.stringify(todoList))
-        },[todoList])
-     } 
-
-
-    
+  // Save todos to localStorage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
 
   return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl '>
-        {/* Title */}
+    <div
+      className="
+        bg-white
+        w-full
+        sm:w-11/12
+        md:w-3/4
+        lg:w-1/3
+        max-w-md
+        mx-auto
+        flex
+        flex-col
+        p-4
+        sm:p-6
+        min-h-[550px]
+        rounded-xl
+        shadow-lg
+      "
+    >
+      {/* Title */}
+      <div className="flex items-center mt-5 gap-2">
+        <img className="w-8" src={to_do} alt="todo icon" />
+        <h1 className="text-2xl sm:text-3xl font-semibold">
+          To-Do List
+        </h1>
+      </div>
 
-        <div  className='flex items-center mt-7 gap-2'>
-            <img className='w-8' src={to_do} alt="" />
-            <h1 className='text-3xl font-semibold'>
-                To-Do List
-            </h1>
-        </div>
-        {/* Input Box  */}
+      {/* Input Box */}
+      <div className="flex flex-col sm:flex-row items-center my-7 bg-gray-200 rounded-full overflow-hidden">
+        <input
+          ref={inputRef}
+          className="
+            bg-transparent
+            border-0
+            outline-none
+            flex-1
+            h-14
+            px-5
+            w-full
+            placeholder:text-slate-600
+          "
+          type="text"
+          placeholder="Add Your Task"
+        />
 
-        <div className='flex items-center my-7 bg-gray-200 rounded-full '>
-            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-5 pr-2 placeholder:text-slate-600' type="text" placeholder='Add Your Task' />
-            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
-        </div>
-        {/* List */}
-    <div>
-        {todoList.map((item,index)=>{
-            return <Todoitems key ={index} text={item.text} id={item.id} isCompelte={item.isCompelte} deleteTodo = {deleteTodo} toggle={toggle} />
+        <button
+          onClick={add}
+          className="
+            bg-orange-600
+            w-full
+            sm:w-32
+            h-14
+            text-white
+            text-lg
+            font-medium
+            cursor-pointer
+          "
+        >
+          ADD +
+        </button>
+      </div>
 
-        })}
-        
-     
-        
+      {/* Todo List */}
+      <div className="flex flex-col gap-3">
+        {todoList.map((item) => (
+          <Todoitems
+            key={item.id}
+            text={item.text}
+            id={item.id}
+            isCompelte={item.isCompelte}
+            deleteTodo={deleteTodo}
+            toggle={toggle}
+          />
+        ))}
+      </div>
     </div>
-
-     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
