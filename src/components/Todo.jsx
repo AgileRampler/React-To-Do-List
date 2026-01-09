d48b18df124a26e16be498ed2079d66056a07443
AgileRampler <43984000+AgileRampler@users.noreply.github.com>
2026-01-09 16:25:37 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -60,33 +60,45 @@ function Todo() {
     
 
   return (
-    <div className='bg-white place-self-center  max-w-md flex flex-col p-7 min-h-[550px]   rounded-xl min-w-min   '>
-        {/* Title */}
-
-        <div  className='flex  items-center mt-7 gap-2'>
-            <img className='w-8' src={to_do} alt="" />
-            <h1 className='text-3xl font-semibold'>
-                To-Do List
-            </h1>
+   <>
+        <div className='bg-white place-self-center  max-w-md flex flex-col p-7  min-h-[550px]   rounded-xl min-w-12   '>
+            {/* Title */}
+    
+            <div  className='flex  items-center mt-7 gap-2'>
+                <img className='w-8' src={to_do} alt="" />
+                <h1 className='text-3xl font-semibold'>
+                    To-Do List
+                </h1>
+            </div>
+            {/* Input Box  */}
+    
+            <div className='flex items-center my-7 bg-gray-200 rounded-full '>
+                <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-5 pr-2 placeholder:text-slate-600' type="text" placeholder='Add Your Task' />
+                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+            </div>
+            {/* List */}
+        <div>
+            {todoList.map((item,index)=>{
+                return <Todoitems key ={index} text={item.text} id={item.id} isCompelte={item.isCompelte} deleteTodo = {deleteTodo} toggle={toggle} />
+    
+            })}
+            
+         
+            
         </div>
-        {/* Input Box  */}
-
-        <div className='flex items-center my-7 bg-gray-200 rounded-full '>
-            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-5 pr-2 placeholder:text-slate-600' type="text" placeholder='Add Your Task' />
-            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
-        </div>
-        {/* List */}
-    <div>
-        {todoList.map((item,index)=>{
-            return <Todoitems key ={index} text={item.text} id={item.id} isCompelte={item.isCompelte} deleteTodo = {deleteTodo} toggle={toggle} />
-
-        })}
+    
+         </div>
         
-     
-        
-    </div>
 
-     </div>
+   
+   </>
+
+
+
+
+
+
+
   )
 }
 
